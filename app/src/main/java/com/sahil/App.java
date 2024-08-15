26e43b43aa65ecb60ca574463136d1d17173054a
Jeel Dobariya <jeeldobariya38@gmail.com>
2024-08-15 12:26:52 +0530

    Create a basic project with Gradle

--- app/src/main/java/com/sahil/App.java
+++ app/src/main/java/com/sahil/App.java
@@ -1,7 +1,7 @@
 package com.sahil;
 
-public class Main {
+public class App {
   public static void main(String[] args) {
-    System.out.println("Hello, World!!")
+    System.out.println("Hello World!");
   }
 }
